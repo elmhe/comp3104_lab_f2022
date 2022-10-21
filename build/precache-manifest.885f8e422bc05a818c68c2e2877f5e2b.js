86db96573a8dc6827c4dfa130dc7fccf35d5952e
Elam <elamhe02@outlook.com>
2022-10-21 00:16:23 -0400

    Update build

---
+++ build/precache-manifest.885f8e422bc05a818c68c2e2877f5e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91c3cf9365a399af2244",
+    "url": "/elmhe/comp3104_lab_f2022/static/css/main.1a11a20e.chunk.css"
+  },
+  {
+    "revision": "91c3cf9365a399af2244",
+    "url": "/elmhe/comp3104_lab_f2022/static/js/main.91c3cf93.chunk.js"
+  },
+  {
+    "revision": "8a0fd26beb29b34abe9a",
+    "url": "/elmhe/comp3104_lab_f2022/static/js/runtime~main.8a0fd26b.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/elmhe/comp3104_lab_f2022/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "4a89d8f1810cdc44d0c8a187e172702a",
+    "url": "/elmhe/comp3104_lab_f2022/index.html"
+  }
+];
